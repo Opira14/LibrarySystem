d565663c06d4402e47427724735788a50b22b0a2
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-12 01:34:19 -0400

    Add all argument and no argument constructor for user class

--- src/main/java/org/piraven/User.java
+++ src/main/java/org/piraven/User.java
@@ -1,5 +1,6 @@
 package org.piraven;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class User {
@@ -7,6 +8,22 @@ public abstract class User {
     protected String name;
     protected List<Item> borrowedItems;
     protected Gender gender;
+
+    private static int nextId = 1;
+
+    public User() {
+        this.id = null;
+        this.name = null;
+        this.borrowedItems = new ArrayList<>();
+        this.gender = null;
+    }
+
+    public User(String name, List<Item> borrowedItems, Gender gender) {
+        this.id = String.format("04d", nextId++);
+        this.name = name;
+        this.borrowedItems = borrowedItems;
+        this.gender = gender;
+    }
 
     public enum Gender {
         MALE, FEMALE
