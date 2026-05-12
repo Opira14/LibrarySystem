b15ebf66c47709e5e8f189303620a3bf9dc8ea81
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-12 00:57:31 -0400

    Create the field for User class

--- src/main/java/org/piraven/User.java
+++ src/main/java/org/piraven/User.java
@@ -1,4 +1,10 @@
 package org.piraven;
 
+import java.util.List;
+
 public abstract class User {
+    protected String id;
+    protected String name;
+    protected List<Item> borrowedItems;
+    protected Gender gender;
 }
