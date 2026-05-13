9fcc83e39b27dbcfe1498a7e4040d6c0da652577
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 18:07:03 -0400

    Add generateReport method to Admin class

--- src/main/java/org/piraven/Admin.java
+++ src/main/java/org/piraven/Admin.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Getter
 @Setter
 @ToString(callSuper = true)
-public class Admin extends User {
+public class Admin extends User implements Reportable {
     public Admin(String name, List<Item> borrowedItems, Gender gender) {
         super(name, borrowedItems, gender);
         this.id = "A" + String.format("%04d", nextId++);
@@ -23,5 +23,29 @@ public class Admin extends User {
     @Override
     public boolean canBorrow(Item item) {
         return false;
+    }
+
+    @Override
+    public void generateReport(List<Item> items) {
+        System.out.println("=== BORROWED-ITEMS ===");
+
+        items.stream()
+                .filter(item -> item.getStatus() == Item.Status.BORROWED)
+                .forEach(item ->
+                        System.out.println(item.getTitle()));
+
+        System.out.println("\n=== INSTORE-ITEMS ===");
+
+        items.stream()
+                .filter(item -> item.getStatus() == Item.Status.INSTORE)
+                .forEach(item ->
+                        System.out.println(item.getTitle()));
+
+        System.out.println("\n=== LOST-ITEMS ===");
+
+        items.stream()
+                .filter(item -> item.getStatus() == Item.Status.LOST)
+                .forEach(item ->
+                        System.out.println(item.getTitle()));
     }
 }
