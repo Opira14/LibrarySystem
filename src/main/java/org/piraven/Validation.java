232b9428af065e84443cb0bc88fc2d90a2058994
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 18:42:03 -0400

    Add isValidName method to validation class

--- src/main/java/org/piraven/Validation.java
+++ src/main/java/org/piraven/Validation.java
@@ -1,4 +1,22 @@
 package org.piraven;
 
 public class Validation {
+    /**
+     * Checks if the name is valid
+     * @param name the given name
+     * @return if the name is valid or not
+     */
+    public static boolean isValidName(String name) {
+        if (name == null) {
+            return false;
+        }
+
+        for (int i = 0; i < name.length(); i++) {
+            if (Character.isLetter(name.charAt(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
