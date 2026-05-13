e7a68a27808f1135bed1d80954be8a01881d666e
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 17:11:44 -0400

    Add constructor to Admin class

--- src/main/java/org/piraven/Admin.java
+++ src/main/java/org/piraven/Admin.java
@@ -4,8 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @Setter
 @ToString(callSuper = true)
 public class Admin extends User {
+    public Admin(String name, List<Item> borrowedItems, Gender gender) {
+        super(name, borrowedItems, gender);
+        this.id = "A" + String.format("%04d", nextId++);
+    }
 }
