19b1e19f66d5a59b5ae54f5d6e14adb2cea0ca13
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 14:52:43 -0400

    Add the borrow method in Item class

--- src/main/java/org/piraven/Item.java
+++ src/main/java/org/piraven/Item.java
@@ -1,5 +1,8 @@
 package org.piraven;
 
+import lombok.Getter;
+
+@Getter
 public abstract class Item {
     protected String id;
     protected String title;
@@ -8,9 +11,16 @@ public abstract class Item {
     private static int nextId = 1;
 
     public Item(String title, Status status) {
-        this.id = String.format("04d", nextId++);
+        this.id = String.format("%04d", nextId++);
         this.title = title;
         this.status = status;
+    }
+
+    /**
+     * marks the item as borrowed
+     */
+    public void borrow() {
+        this.status = Status.BORROWED;
     }
 
     public enum Status {
