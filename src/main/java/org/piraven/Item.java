db1e0b319bdba105acc82170f3c53439526ade9e
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 11:48:53 -0400

    Add fields and enum class for Item class

--- src/main/java/org/piraven/Item.java
+++ src/main/java/org/piraven/Item.java
@@ -1,4 +1,11 @@
 package org.piraven;
 
 public abstract class Item {
+    protected String id;
+    protected String title;
+    protected Status status;
+
+    public enum Status {
+        BORROWED, INSTORE, LOST
+    }
 }
