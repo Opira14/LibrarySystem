56e98860afa276c34f1fedf5672499a242b3446d
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 19:16:47 -0400

    Add comparator to Student class

--- src/main/java/org/piraven/Student.java
+++ src/main/java/org/piraven/Student.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Getter
 @Setter
 @ToString(callSuper = true)
-public class Student extends User {
+public class Student extends User implements Comparator<Student> {
     public Student(String name, List<Item> borrowedItems, Gender gender) {
         super(name, borrowedItems, gender);
         this.id = "S" + String.format("%04d", nextId++);
@@ -27,5 +28,22 @@ public class Student extends User {
         }
 
         return item.getStatus() == Item.Status.INSTORE;
+    }
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        String field = "";
+
+        switch (field.toLowerCase()) {
+            case "id" -> {
+                return o1.id.compareTo(o2.id);
+            }
+
+            case "name" -> {
+                return o1.name.compareTo(o2.name);
+            }
+        }
+
+        return 0;
     }
 }
