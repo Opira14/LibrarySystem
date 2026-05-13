b1fb7e7962200e123235ffd68a1cdc9ef0ca017f
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 17:03:02 -0400

    Add constructor to Student class

--- src/main/java/org/piraven/Student.java
+++ src/main/java/org/piraven/Student.java
@@ -4,9 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @Setter
 @ToString(callSuper = true)
 public class Student extends User {
-
+    public Student(String name, List<Item> borrowedItems, Gender gender) {
+        super(name, borrowedItems, gender);
+        this.id = "S" + String.format("%04d", nextId++);
+    }
 }
