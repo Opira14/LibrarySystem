105f7a4acb99f1c34e2253f0b2e7f7cfec9f610b
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 02:26:14 -0400

    Update the User class constructor

--- src/main/java/org/piraven/User.java
+++ src/main/java/org/piraven/User.java
@@ -11,17 +11,10 @@ public abstract class User {
 
     private static int nextId = 1;
 
-    public User() {
-        this.id = null;
-        this.name = null;
-        this.borrowedItems = new ArrayList<>();
-        this.gender = null;
-    }
-
     public User(String name, List<Item> borrowedItems, Gender gender) {
         this.id = String.format("04d", nextId++);
         this.name = name;
-        this.borrowedItems = borrowedItems;
+        this.borrowedItems = new ArrayList<>();
         this.gender = gender;
     }
 
