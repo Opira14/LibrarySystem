6eeef6c606228cf34760288ebef5599500139cc5
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 11:50:17 -0400

    Add constructor for Item class

--- src/main/java/org/piraven/Item.java
+++ src/main/java/org/piraven/Item.java
@@ -5,6 +5,14 @@ public abstract class Item {
     protected String title;
     protected Status status;
 
+    private static int nextId = 1;
+
+    public Item(String title, Status status) {
+        this.id = String.format("04d", nextId++);
+        this.title = title;
+        this.status = status;
+    }
+
     public enum Status {
         BORROWED, INSTORE, LOST
     }
