ca0ccfc7a18c0ff98e6f7c9359ee2b65db3b9c34
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 19:31:06 -0400

    Add comparator for Teacher class

--- src/main/java/org/piraven/Teacher.java
+++ src/main/java/org/piraven/Teacher.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Getter
@@ -25,5 +26,31 @@ public class Teacher extends User {
     public boolean canBorrow(Item item) {
         return item != null
                 && item.getStatus() == Item.Status.INSTORE;
+    }
+
+    public static class TeacherComparator implements Comparator<Teacher> {
+
+        private final String field;
+
+        public TeacherComparator(String field) {
+            this.field = field;
+        }
+
+        @Override
+        public int compare(Teacher o1, Teacher o2) {
+
+            switch (field.toLowerCase()) {
+
+                case "id" -> {
+                    return o1.getId().compareTo(o2.getId());
+                }
+
+                case "name" -> {
+                    return o1.getName().compareTo(o2.getName());
+                }
+
+                default -> throw new IllegalArgumentException("Invalid field: " + field);
+            }
+        }
     }
 }
