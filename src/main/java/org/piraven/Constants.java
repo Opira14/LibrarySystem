6bf2df0141902ff9d0ebe1b46fc893a85efcc5e3
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 16:16:35 -0400

    Add fields for Constants class

--- src/main/java/org/piraven/Constants.java
+++ src/main/java/org/piraven/Constants.java
@@ -1,4 +1,8 @@
 package org.piraven;
 
 public class Constants {
+    public static final int MAX_BOOKS_STUDENT = 5;
+    public static final int MAX_ITEM_TEACHER = 10;
+    public static final String BOOKS_CSV_PATH    = "src/main/resources/items.csv";
+    public static final String USERS_CSV_PATH    = "src/main/resources/users.csv";
 }
