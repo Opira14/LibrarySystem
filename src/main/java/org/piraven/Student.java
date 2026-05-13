042639324d8864af92632dd70373b45d7d619e2a
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 17:09:45 -0400

    Add getBorrowLimit and canBorrow method to Student class

--- src/main/java/org/piraven/Student.java
+++ src/main/java/org/piraven/Student.java
@@ -14,4 +14,18 @@ public class Student extends User {
         super(name, borrowedItems, gender);
         this.id = "S" + String.format("%04d", nextId++);
     }
+
+    @Override
+    public int getBorrowLimit() {
+        return Constants.MAX_BOOKS_STUDENT;
+    }
+
+    @Override
+    public boolean canBorrow(Item item) {
+        if (!(item instanceof Book)) {
+            return false;
+        }
+
+        return item.getStatus() == Item.Status.INSTORE;
+    }
 }
