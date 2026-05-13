efba0835f3629a181ac5ed82305d8ba247228eb9
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 12:00:51 -0400

    Add fields and enum for Book class

--- src/main/java/org/piraven/Book.java
+++ src/main/java/org/piraven/Book.java
@@ -1,4 +1,13 @@
 package org.piraven;
 
 public class Book extends Item {
+    private String ISBN;
+    private String author;
+    private Genre genre;
+
+    public enum Genre {
+        SCIENCEFICTION, MYSTERY, LITERARYFICTION, HORROR, HISTORICALFICTION, THRILLER,
+        ROMANCE, HISTORY, FANTASY, BIOGRAPHY, WESTERN, BILDUNGSROMAN,
+        DYSTOPIAN, MAGICALREALISM, REALISTLITERATURE
+    }
 }
