c41739d9fd789f524a79391ebc7093aebffd0ac1
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-14 19:16:12 -0400

    Fix the error in Constants class

--- src/main/java/org/piraven/Constants.java
+++ src/main/java/org/piraven/Constants.java
@@ -3,6 +3,6 @@ package org.piraven;
 public class Constants {
     public static final int MAX_BOOKS_STUDENT = 5;
     public static final int MAX_ITEM_TEACHER = 10;
-    public static final String ITEMS_CSV_PATH = "C:\\Users\\srisu\\IdeaProjects\\LibrarySystem\\src\\main\\resources\\Item.csv";
-    public static final String USERS_CSV_PATH = "C:\\Users\\srisu\\IdeaProjects\\LibrarySystem\\src\\main\\resources\\User.csv";
+    public static final String ITEMS_CSV_PATH = "src/main/resources/Item.csv";
+    public static final String USERS_CSV_PATH = "src/main/resources/User.csv";
 }
