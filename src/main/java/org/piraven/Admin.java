af8519fa6ab221c48b8c79721fe512e1124e8461
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-14 15:08:28 -0400

    Update the Admin class

--- src/main/java/org/piraven/Admin.java
+++ src/main/java/org/piraven/Admin.java
@@ -50,48 +50,4 @@ public class Admin extends User implements Reportable {
                 .forEach(item ->
                         System.out.println(item.getTitle()));
     }
-    /**
-     * Admin triggers system backup.
-     * Saves all users and items into CSV files.
-     */
-    public void backupData(Library library) {
-        try (FileWriter writer = new FileWriter(Constants.USERS_CSV_PATH)) {
-            for (User user : library.getUsers().values()) {
-                String userType = user.getClass().getSimpleName();
-
-                List<String> borrowedIds = new ArrayList<>();
-
-                for (Item item : user.getBorrowedItems()) {
-                    borrowedIds.add(item.getId());
-                }
-
-                String borrowedIdsString = String.join(";", borrowedIds);
-                writer.write(user.getId() + "," + user.getName() + "," + userType + "," +
-                        borrowedIdsString + "," + user.getGender() + "\n");
-            }
-
-        } catch (Exception e) {
-            System.out.println("Failed to backup users: " + e.getMessage());
-        }
-
-        try (FileWriter writer = new FileWriter(Constants.ITEMS_CSV_PATH)) {
-            for (Item item : library.getItems()) {
-                if (item instanceof Book book) {
-                    writer.write(book.getId() + "," + book.getClass().getSimpleName() + "," + book.getTitle() + "," +
-                            book.getStatus() + "," + book.getISBN() + "," + book.getAuthor() + "," + book.getGenre() + "\n");
-                } else if (item instanceof DVD dvd) {
-                    writer.write(dvd.getId() + "," + dvd.getClass().getSimpleName() + "," + dvd.getTitle() + "," +
-                            dvd.getStatus() + "," + dvd.getDirector() + "," + dvd.getDuration() + "\n");
-                } else if (item instanceof Magazine mag) {
-                    writer.write(mag.getId() + "," + mag.getClass().getSimpleName() + "," + mag.getTitle() + "," +
-                            mag.getStatus() + "," + mag.getIssueNumber() + "," + mag.getPublisher() + "\n");
-                }
-            }
-
-        } catch (Exception e) {
-            System.out.println("Failed to backup items: " + e.getMessage());
-        }
-
-        System.out.println("Backup completed.");
-    }
 }
