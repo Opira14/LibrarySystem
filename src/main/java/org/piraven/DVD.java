45f2a185c6bca9544775a92c69b9c92d06ae1e6c
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2026-05-13 20:43:32 -0400

    Add constructor for Magazine and DVD class

--- src/main/java/org/piraven/DVD.java
+++ src/main/java/org/piraven/DVD.java
@@ -10,4 +10,10 @@ import lombok.ToString;
 public class DVD extends Item {
     private String director;
     private int duration;
+
+    public DVD(String title, Status status, String director, int duration) {
+        super(title, status);
+        this.director = director;
+        this.duration = duration;
+    }
 }
--- src/main/java/org/piraven/Magazine.java
+++ src/main/java/org/piraven/Magazine.java
@@ -10,4 +10,10 @@ import lombok.ToString;
 public class Magazine extends Item {
     private int issueNumber;
     private String publisher;
+
+    public Magazine(String title, Status status, int issueNumber, String publisher) {
+        super(title, status);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
